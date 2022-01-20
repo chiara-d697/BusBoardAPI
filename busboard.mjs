824ad018ab0225aac5a752d3551f128c5dc6552f
Chiara Dinan <Chiara.Dinan@softwire.com>
2022-01-20 14:35:16 +0000

    ordered json response according to time to station

--- busboard.mjs
+++ busboard.mjs
@@ -11,5 +11,12 @@ let userStopCode = prompt("Please enter a bus stop code:")
 fetch(`https://api.tfl.gov.uk/StopPoint/${userStopCode}/Arrivals`
     )
     .then(response => response.json())
-    .then(body => console.log(body));
+    .then(body => {
+        body.sort((b1, b2) => b1.timeToStation - b2.timeToStation);
 
+
+        for(const bus of body) {
+            console.log(`Bus ${bus.lineId} is coming in ${Math.floor(bus.timeToStation/60)} minutes and ${bus.timeToStation%60} seconds`);
+        }
+
+       }); 
