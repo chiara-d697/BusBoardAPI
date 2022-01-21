d9952463dc3052f5a7afb5d94492a8a3143c40d9
Chiara Dinan <Chiara.Dinan@softwire.com>
2022-01-21 12:03:43 +0000

    working on wiring the two APIs together

--- busboardpostcodes.mjs
+++ busboardpostcodes.mjs
@@ -11,17 +11,17 @@ let userPostcode = prompt("Please enter your postcode: ")
 async function fetchPostcodeInfo() {
     const postcode = await fetch(`https://api.postcodes.io/postcodes?q=${userPostcode}`);
     const postcodeInfo = await postcode.json();
+    const lat = postcodeInfo.result[0].latitude;
+    const lon = postcodeInfo.result[0].longitude;
 
-    postcodeInfo.result[0].longitude
-
-    const buses = await fetch(`https://api.tfl.gov.uk/StopPoint?stopTypes=NaptanPublicBusCoachTram&lat=
-    ${postcodeInfo.result[0].latitude}&lon=${postcodeInfo.result[0].longitude}`)
+    const stopTypes = "NaptanOnstreetBusCoachStopPair,NaptanPublicBusCoachTram";
+    const buses = await fetch(`https://api.tfl.gov.uk/StopPoint?stopTypes=${stopTypes}&lat=
+${lat}&lon=${lon}&radius=200`)
     const busesInfo = await buses.json();
 
 
-    console.log(`Your nearest bus stop is ${busesInfo.stopPoints[0].commonName}`);
+    // console.log(`Your nearest bus stop is ${busesInfo.stopPoints[0].commonName}`);
 }
 
 
     fetchPostcodeInfo();
-
